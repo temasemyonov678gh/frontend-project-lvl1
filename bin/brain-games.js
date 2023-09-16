308c50bb0f48cd1d5c06fc99cddd5c8e4a79a2da
Vitaliy <morozov.vitalick2016@yandex.ru>
2023-09-16 18:32:48 +0500

    bug fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import { sayName } from '../src/cli.js';
+import sayName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-sayName();
+const userName = sayName();
+export default userName;
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const sayName = () => {
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`);
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
-export { sayName };
+export default sayName;
