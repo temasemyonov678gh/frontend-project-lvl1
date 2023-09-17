010ed6ca9fa6dca3b7a37e793627f92bc4b85afd
Vitaliy <morozov.vitalick2016@yandex.ru>
2023-09-17 11:39:23 +0500

    fix bugs

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/temasemyonov678gh/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/temasemyonov678gh/frontend-project-lvl1/actions)
-### asciinema
+### asciinema brain-even
 https://asciinema.org/a/k8wmipAoVQ9gwqF2LAPnqil6a
+### asciinema brain-calc
+https://asciinema.org/a/GgWTR0onoyxhz9uD3qdMiyzk6
--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -19,7 +19,7 @@ const brainCalc = () => {
         const operand1 = Math.round(Math.random() * 100);
         const operand2 = Math.round(Math.random() * 100);
         let result = 0;
-        let answer = 0;
+        let answer;
         if (operator === '*') {
             result = operand1 * operand2;
             answer = readlineSync.question(`Question: ${operand1} * ${operand2}\nYour answer: `);
@@ -32,13 +32,18 @@ const brainCalc = () => {
             result = operand1 - operand2;
             answer = readlineSync.question(`Question: ${operand1} - ${operand2}\nYour answer: `);
         }
-        if (result === Number.answer) {
+        if (isNaN(Number(answer))) {
+            console.log(`Read game instruction!`);
+            break;
+        }
+        if (result === Number(answer)) {
             console.log('Correct!');
             count += 1;
         } else {
             console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`);
             break;
         }
+        
     }
     if (count === 3) {
         console.log(`Congratulations, ${userName}!`);
