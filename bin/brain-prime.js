7c9a8d6b085691f76f0e597fb0c7188cc9dd8ebf
Vitaliy <morozov.vitalick2016@yandex.ru>
2023-09-18 17:27:08 +0500

    add brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,3 @@
+import brainPrime from  '../src/games/prime.js';
+
+brainPrime();
---
+++ src/games/prime.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import userName from '../../bin/brain-games.js';
+import { randomNum } from '../random.js';
+
+const isPrime = (num) => {
+    const number = Number(num);
+    for (let i = 2; i < number; i += 1) {
+        if (number % i === 0) {
+            return 'no';
+        } 
+    }
+    return 'yes';
+}
+
+const brainPrime = () => {
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+    let count = 0;
+    for (let i = 1; i <= 3; i += 1) {
+        const number = randomNum();
+        const result = isPrime(number);
+        const answer = readlineSync.question(`Question: ${number}\nYour answer: `);
+        if (result === answer) {
+            console.log('Correct!');
+            count += 1;
+        } else {
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`);
+            break;
+        }
+    }
+    if (count === 3) {
+        console.log(`Congratulations, ${userName}!`);
+    }
+};
+export default brainPrime;
