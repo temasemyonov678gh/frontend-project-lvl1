369eb4d423a376ccea1e7a891be1d0127102087a
Vitaliy <morozov.vitalick2016@yandex.ru>
2023-09-18 14:51:20 +0500

    bug fix

---
+++ src/games/gcd.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+import userName from '../../bin/brain-games.js';
+import { randomNum } from '../random.js';
+
+const gcd = (operand1, operand2) => {
+    let a = operand1;
+    let b = operand2;
+    let gcd;
+    while (a !== b) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+    }
+    gcd = a;
+    return gcd;
+};
+
+const brainGcd = () => {
+    console.log('Find the greatest common divisor of given numbers.');
+    let count = 0;
+    for (let i = 1; i <= 3; i += 1) {
+        const operand1 = randomNum();
+        const operand2 = randomNum();
+        const result = gcd(operand1, operand2);
+        const answer = readlineSync.question(`Question: ${operand1} ${operand2}\nYour answer: `);
+        if (isNaN(Number(answer))) {
+            console.log(`Read game instruction!`);
+            break;
+        }
+        if (result === Number(answer)) {
+            console.log('Correct!');
+            count += 1;
+        } else {
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`);
+            break;
+        }
+    }
+    if (count === 3) {
+        console.log(`Congratulations, ${userName}!`);
+    }
+};
+export default brainGcd;
